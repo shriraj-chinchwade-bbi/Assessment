e362365f8bd4c226d6a07f76f0e3aac986408549
shriraj-chinchwade-bbi <141905103+shriraj-chinchwade-bbi@users.noreply.github.com>
2023-08-11 10:11:34 +0530

    Update script.js

--- assignment4/script.js
+++ assignment4/script.js
@@ -11,3 +11,17 @@ first.addEventListener("click", function (event) {
 
   third.style.backgroundColor = "white";
 });
+second.addEventListener("click", function (event) {
+  first.style.backgroundColor = "white";
+
+  second.style.backgroundColor = "black";
+
+  third.style.backgroundColor = "white";
+});
+third.addEventListener("click", function (event) {
+  first.style.backgroundColor = "white";
+
+  second.style.backgroundColor = "white";
+
+  third.style.backgroundColor = "black";
+});
